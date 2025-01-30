7fc715fd18971c75b652628fa3b1155a91281574
giselledunine <sophia.hmamouche@outlook.com>
2025-01-30 18:48:48 +0100

    feat: add elements for later

---
+++ components/ShadereTest.tsx
@@ -0,0 +1,17 @@
+{
+    /* <mesh>
+                    <sphereGeometry args={[1, 32, 16]}></sphereGeometry>
+                    <shaderMaterial
+                        vertexShader={particlesVertexShader}
+                        fragmentShader={particlesFragmentShader}
+                        uniforms={{
+                            uSize: new THREE.Uniform(0.4),
+                            uResolution: new THREE.Uniform(
+                                new THREE.Vector2(
+                                    sizes.width ?? 0 * (sizes.pixelRatio ?? 0),
+                                    sizes.height ?? 0 * (sizes.pixelRatio ?? 0)
+                                )
+                            ),
+                        }}></shaderMaterial>
+                </mesh> */
+}
